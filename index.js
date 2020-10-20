0a89a74d4e5717dafdf50ff8183c5a6feb3e0722
rhammock1 <rhamm1027@gmail.com>
2020-10-20 14:05:22 -0400

    more functions

--- index.js
+++ index.js
@@ -1,56 +1,95 @@
-function addItemToList() {
+const STORE = [
+  {id: cuid(), name: "apples", checked: false},
+  {id: cuid(), name: "oranges", checked: false},
+  {id: cuid(), name: "milk", checked: true},
+  {id: cuid(), name: "bread", checked: false}
+];
+
+
+function generateItemElement(item) {
+  return `
+    <li data-item-id="${item.id}">
+      <span class="shopping-item js-shopping-item ${item.checked ? "shopping-item__checked" : ''}">${item.name}</span>
+      <div class="shopping-item-controls">
+        <button class="shopping-item-toggle js-item-toggle">
+            <span class="button-label">check</span>
+        </button>
+        <button class="shopping-item-delete js-item-delete">
+            <span class="button-label">delete</span>
+        </button>
+      </div>
+    </li>`;
+}
+
+
+function generateShoppingItemsString(shoppingList) {
+  const items = shoppingList.map((item) => generateItemElement(item));
+  
+  return items.join("");
+}
+
+
+function renderShoppingList() {
+  const shoppingListItemsString = generateShoppingItemsString(STORE);
+
+  $('.shopping-list').html(shoppingListItemsString);
+}
+
+
+function addItemToShoppingList(itemName) {
+  STORE.push({id: cuid(), name: itemName, checked: false});
+}
+
+function handleNewItemSubmit() {
   $('#js-shopping-list-form').on('submit', function(event) {
     event.preventDefault();
-    
-    let listItem = $('#shopping-list-entry').val();
-    //console.log(listItem);
-    let template = `<li>
-        <span class="shopping-item">${listItem}</span>
-        <div class="shopping-item-controls">
-          <button class="shopping-item-toggle">
-            <span class="button-label">check</span>
-          </button>
-          <button class="shopping-item-delete">
-            <span class="button-label">delete</span>
-          </button>
-        </div>
-      </li>`;
-    $('.shopping-list').append(template);
-    this.reset();
-    //console.log(this);
+    const newItemName = $('#shopping-list-entry').val();
+    $('#shopping-list-entry').val('');
+    addItemToShoppingList(newItemName);
+    renderShoppingList();
   });
 }
 
-function markOffOn() {
-    //event delegation
-  // parent element                   child selector
-  $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
-    
-    //console.log('the button was clicked');
+function toggleCheckedForListItem(itemId) {
+  const item = STORE.find(item => item.id === itemId);
+  item.checked = !item.checked;
+}
 
-    //both below work, but children[0] is most specific. script could
-    //break though if children[0] changes/moves
 
-    //$(this).parent().siblings().toggleClass('shopping-item__checked');
-    $(this.closest('li').children[0]).toggleClass('shopping-item__checked');
-    
+function getItemIdFromElement(item) {
+  return $(item)
+    .closest('li')
+    .data('item-id');
+}
+
+function handleItemCheckClicked() {
+  $('.shopping-list').on('click', `.shopping-item-toggle`, event => {
+    const id = getItemIdFromElement(event.currentTarget);
+    toggleCheckedForListItem(id);
+    renderShoppingList();
   });
 }
 
-function deleteItem() {
-  //event delegation
-  // parent element                   child selector
-  $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
-    //console.log('the DELETE button was clicked');
-    $(this).closest('li').remove();
-    
+function deleteItem(itemId) {
+  const itemIndex = STORE.findIndex(item => item.id === itemId);
+  STORE.splice(itemIndex, 1);
+}
+
+function handleDeleteItemClicked() {
+  $('.shopping-list').on('click', '.shopping-item-delete', event => {
+    const itemId = getItemIdFromElement(event.currentTarget);
+    deleteItem(itemId);
+    renderShoppingList();
   });
+
 }
 
-function startUp() {
-  addItemToList();
-  markOffOn();
-  deleteItem();
+function handleShoppingList() {
+  renderShoppingList();
+  handleNewItemSubmit();
+  handleItemCheckClicked();
+  handleDeleteItemClicked();
 }
 
-$(startUp);
+// when the page loads, call `handleShoppingList`
+$(handleShoppingList);
